8279f3adad884da9a2ac29460d17601f548d1d71
amanduke <65379991+amanduke@users.noreply.github.com>
2020-10-18 14:54:32 -0400

    server

--- develop/public/assets/server.js
+++ develop/public/assets/server.js
@@ -5,9 +5,9 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const fs = require('fs');
 const express = require('express');
 
-// const PORT = 3001;
+// const PORT = 5500;
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5500;
 
 
 
