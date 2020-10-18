ef12d6d83cbd244dea9344a37efba2fb79d1a3c0
amanduke <65379991+amanduke@users.noreply.github.com>
2020-10-17 23:15:33 -0400

    modified

--- develop/routes/apiRoutes/index.js
+++ develop/routes/apiRoutes/index.js
@@ -1,8 +1,14 @@
-const router = require('express').Router();
-const noteRoutes = require('../apiRoutes/noteRoutes');
+var path = require("path");
+var router = require("express").Router();
 
-router.use(noteRoutes);
-// router.use(require('./zookeeperRoutes'));
+// "/notes" responds with the notes.html file
+router.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
+// All other routes respond with the index.html file
+router.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = router;
